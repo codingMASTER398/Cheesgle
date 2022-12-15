d73eacc3ded8d8865df322e075ab6a8beddb3c83
coding398 <55938439+codingMASTER398@users.noreply.github.com>
2022-12-15 07:42:24 +0000

    Setting up version control in Replit

--- saveDatabase.js
+++ saveDatabase.js
@@ -1,9 +1,9 @@
-const { parentPort, workerData } = require('worker_threads');
-var jsonpack = require("jsonpack")
-var fs = require("fs")
+const { parentPort, workerData } = require("worker_threads");
+var jsonpack = require("jsonpack");
+var fs = require("fs");
 
-fs.writeFileSync("./dbb.txt",jsonpack.pack(workerData))
-fs.writeFileSync("./db.txt",jsonpack.pack(workerData))
+fs.writeFileSync("./dbb.txt", jsonpack.pack(workerData));
+fs.writeFileSync("./db.txt", jsonpack.pack(workerData));
 
 parentPort.postMessage(true);
-process.exit()
+process.exit();
